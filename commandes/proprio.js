2e1c8c0e45509237bcb97f5c7b7f69a713c521f4
senal2 <97817147+senal2@users.noreply.github.com>
2024-11-14 06:09:02 +0530

    Update proprio.js

--- commandes/proprio.js
+++ commandes/proprio.js
@@ -14,7 +14,7 @@ const{repondre,ms,dev,superUser}=com;
 
   if(!superUser)
   {
-    return repondre("This command is for *Gojou-md* owner only");
+    return repondre("This command is for *Senal-MD* owner only");
   }
 
   const {exec}=require("child_process")
